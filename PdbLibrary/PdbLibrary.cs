a29b619b84e8ecfb7eb4a4aa55be81e1d720ab4b
Dave Plunkett <dave@bugsplatsoftware.com>
2018-04-02 21:17:53 +0000

    Cleaned up with variable names and comments from LLVM site that describes the PDB file formation layout.
    [git-p4: depot-paths = "//depot/src/": change = 3223]

--- PdbLibrary/PdbLibrary.cs
+++ PdbLibrary/PdbLibrary.cs
@@ -50,38 +50,6 @@ namespace PdbLibrary
         };
     }
 
-    //class Program
-    //{
-
-    //    static void Main(string[] args)
-    //    {
-    //        if (args.Count() < 1)
-    //        {
-    //            Console.WriteLine("Usage: PdbSignature file.pdb");
-    //            Environment.Exit(-1);
-    //        }
-
-    //        if (BitConverter.IsLittleEndian != true)
-    //        {
-    //            Console.WriteLine("Big Endian architectures not supported");
-    //            Environment.Exit(-2);
-    //        }
-
-    //        string ext = Path.GetExtension(args[0]);
-    //        if (String.Equals(ext, ".pdb", StringComparison.OrdinalIgnoreCase))
-    //        {
-    //            PDBFile pdbFile = new PDBFile(args[0]);
-    //            Console.WriteLine(String.Format("PDB signature is {0}", pdbFile.guid.Value()));
-    //        }
-    //        else
-    //        {
-    //            PEFile peFile = new PEFile(args[0]);
-    //            Console.WriteLine(String.Format("PE signature is {0}", peFile.guid()));
-    //        }
-
-    //        Environment.Exit(0);
-    //    }
-    //}
 
     public static class ReadHelper
     {
@@ -137,199 +105,130 @@ namespace PdbLibrary
         // A bare bones abstraction of the root stream of an PDB files. Provides
         // methods to read raw chunks of the root stream, as well as accessing
         // data about streams containing streams.
-
         FileStream fp;
-        uint page_size;
-        uint size;
-        uint[] page_index;
+        uint blockSize;
+        uint numDirectoryBytes;
+        uint[] streamDirectory;
+        uint numStreams;
 
-        public Root(FileStream fp, uint page_size, uint size)
+        public Root(FileStream fp)
         {
             this.fp = fp;
-            this.page_size = page_size;
-            this.size = size;
-            this.page_index = null;
+            this.ReadData();
         }
 
-        public uint pages(uint size, uint page_size)
+        public uint getBlockSize() { return blockSize; }
+
+        public uint numBlocksRequired(uint numBytes, uint blockSize)
         {
-            // calculate number of pages that are required to store the specified number of bytes
-            // param size: number of bytes
-            // param page_size: page size
-            return (uint)(Math.Ceiling((float)size / page_size));
+            // returns number of blocks that are required to store the specified number of bytes
+            return (uint)(Math.Ceiling((float)numBytes / blockSize));
         }
 
-        public Boolean load_pages()
+        public Boolean ReadData()
         {
-            // get page numbers used by the root stream
-            uint num_pages = pages(size, page_size);
+            // LLVM.org refers to the first section of the file as The Superblock
+            // At file offset 0 in an MSF file is the MSF SuperBlock, which is laid out as follows:
+            //
+            // struct SuperBlock {
+            //  char FileMagic[sizeof(Magic)];
+            //  ulittle32_t BlockSize;
+            //  ulittle32_t FreeBlockMapBlock;
+            //  ulittle32_t NumBlocks;
+            //  ulittle32_t NumDirectoryBytes;
+            //  ulittle32_t Unknown;
+            //  ulittle32_t BlockMapAddr;
+            // };
 
-            // root stream indexes starts 5 int pointers after the signature
-            uint num_root_index_pages = pages(num_pages * sizeof(int), page_size);
+            uint fileReadOffset = (uint)PdbSignature.signature.Length;
+            blockSize = ReadHelper.ReadUInt32(fp, fileReadOffset);
 
-            // Something not right here....
-            //uint num_root_index_pages = num_pages;
+            fileReadOffset = (uint)PdbSignature.signature.Length + 3 * sizeof(int);
+            numDirectoryBytes = ReadHelper.ReadUInt32(fp, fileReadOffset);
+            uint numBlockMapAddrs = numBlocksRequired(numDirectoryBytes, blockSize);    // number of uints at blockMapAddr
+           
+            // directory stream address is 5 int pointers after the signature
+            fileReadOffset = (uint)PdbSignature.signature.Length + 5 * sizeof(int);
+            uint blockMapAddr = ReadHelper.ReadUInt32(fp, fileReadOffset);        
+            uint[] blockMapPages = ReadHelper.ReadUInt32Array(fp, blockMapAddr*blockSize, numBlockMapAddrs);
 
-            uint fileReadOffset = (uint)PdbSignature.signature.Length + 5 * sizeof(int);
+            // read in the bytes identifed by the blockMapPages
+            byte[] root_page_data = new byte[numBlockMapAddrs * blockSize];
 
-            // have to read in pages????
-            // somthing like... uint[] root_index_pages = readPageUInts(fileReadOffset, num_root_index_pages);
-            uint[] root_index_pages = ReadHelper.ReadUInt32Array(fp, fileReadOffset, num_root_index_pages);
-            for (int i = 0; i < num_root_index_pages; i++)
+            for (int i=0; i < numBlockMapAddrs; i++)
             {
-                Console.WriteLine(String.Format("root_index_pages[{0}]={1}", i, root_index_pages[i]));
-            }
-
-            // read in the root page list
-            //root_page_data = b""
-            //for root_index in root_index_pages:
-            //    self.fp.seek(root_index* self.page_size)
-            //    root_page_data += self.fp.read(self.page_size)
-            byte[] root_page_data = new byte[num_root_index_pages * page_size];
-            for (int i = 0; i < num_root_index_pages; i++)
-            {
-                uint root_index = root_index_pages[i];
-                fileReadOffset = root_index * this.page_size;
+                uint root_index = blockMapPages[i];
+                fileReadOffset = root_index * this.blockSize;
                 fp.Seek(fileReadOffset, SeekOrigin.Begin);
-                int n = fp.Read(root_page_data, i * (int)page_size, (int)page_size);
-                if (n != page_size)
+                int n = fp.Read(root_page_data, i * (int)blockSize, (int)blockSize);
+                if (n != blockSize)
                 {
-                    Console.WriteLine("Error reading root pages");
-                    Environment.Exit(-1);
+                    throw new Exception(
+                        String.Format("Error reading root pages {0} != {1} at index {3}",
+                            n, blockSize, i));
                 }
             }
 
-            //page_list_fmt = "<" + ("%dI" % num_pages)
-            //return struct.unpack(page_list_fmt, root_page_data[:num_pages * 4])
-            page_index = new uint[num_pages];
-            for (int i = 0; i < num_pages; i++)
+            // Finally, read in the stream directory.  It's a block of uints with the following structure.
+            // We store this in a single uint array.
+            //
+            //    struct StreamDirectory {
+            //      ulittle32_t NumStreams;
+            //      ulittle32_t StreamSizes[NumStreams];
+            //      ulittle32_t StreamBlocks[NumStreams][];
+            //    };
+            int streamDirectoryIntLength = (int)numDirectoryBytes / sizeof(int);
+            streamDirectory = new uint[streamDirectoryIntLength];
+            for (int i = 0; i < streamDirectoryIntLength; i++)
             {
-                page_index[i] = BitConverter.ToUInt32(root_page_data, i * 4);
-                Console.WriteLine(String.Format("page_index[{0}]={1}", i, page_index[i]));
+                streamDirectory[i] = BitConverter.ToUInt32(root_page_data, i * 4);
             }
+
+            this.numStreams = streamDirectory[0];    // Number of streams is first entry in streamDirectory
 
             return true;
         }
 
-        public uint[] _pages()
+        uint [] readStreamDirectoryUInts( uint offset, uint num )
         {
-            if (this.page_index == null)
+            uint [] uints = new uint[num];
+            for (int i = 0; i < num; i++ )
             {
-                load_pages();
-            }
-            return this.page_index;
-        }
-
-
-        public uint page_seek(uint page, uint page_offset)
-        {
-            // move the root stream to a new position
-            // :param page: move to this page
-            // :param page_offset: move to the offset in the specified page
-            uint offset = (_pages()[page]) * page_size + page_offset;
-            fp.Seek(offset, SeekOrigin.Begin);
-
-            return offset;
-        }
-
-        public uint readPageUInt(uint start)
-        {
-            byte[] bytes = readPageBytes(start, sizeof(int));
-            uint rval = BitConverter.ToUInt32(bytes, 0);
-            return rval;
-        }
-
-        public uint[] readPageUInts(uint start, int length)
-        {
-            byte[] bytes = readPageBytes(start, length * sizeof(int));
-            uint[] uints = new uint[length];
-
-            for (int i = 0; i < length; i++)
-            {
-                uints[i] = BitConverter.ToUInt32(bytes, i * sizeof(int));
+                uints[i] = streamDirectory[offset + i];
             }
 
             return uints;
         }
 
-        public byte[] readPageBytes(uint start, int length)
-        {
-            // read root stream bytes
-            // :param start: the global offset where to read
-            // :param length: number of bytes to read
-            // :return: root bytes as an bytes array
-
-            uint start_page = start / this.page_size;    // Floor division
-            uint start_byte = start % this.page_size;    // Modulo operator
-
-            int partial_size = (int)Math.Min(length, page_size - start_byte);
-            byte[] bytes = new byte[length];
-            this.page_seek(start_page, start_byte);
-            int n = fp.Read(bytes, 0, partial_size);
-            int bytesRead = partial_size;
-            length -= partial_size;
-
-            while (0 < length)
-            {
-                Console.WriteLine("Warning: seldom used code block");
-                start_page += 1;
-                this.page_seek(start_page, 0);
-                partial_size = Math.Min((int)page_size, length);
-                n = fp.Read(bytes, bytesRead, partial_size);
-                bytesRead += partial_size;
-                length -= partial_size;
-            }
-
-            return bytes;
-        }
-
-        public uint num_streams()
-        {
-            // get number of streams listed in this root stream
-            //return struct.unpack("<I", self.read(0, 4))[0]
-            uint num_streams = readPageUInt(0);
-            return num_streams;
-        }
-
-
         public uint stream_size(uint stream_index)
         {
-            // get stream's size in bytes
-            if (stream_index >= this.num_streams())
+            // get stream's numDirectoryBytes in bytes
+            if (stream_index >= numStreams)
             {
-                Console.Write("stream index to large");
-                Environment.Exit(-1);
+                throw new Exception(String.Format(
+                    "stream index to large: {0}, {1}", stream_index, numStreams));
             }
 
-            // return struct.unpack("<I", self.read(stream_index* 4 + 4, 4))[0]
-            uint size = readPageUInt(stream_index * 4 + 4);
+            uint size = streamDirectory[1 + stream_index];
             return size;
         }
 
         public uint[] stream_pages(uint stream_index)
         {
             // get stream's page numbers
-            uint num_streams = this.num_streams();
-            Console.WriteLine(String.Format("num_streams {0}", num_streams));
-            uint pages_offset = 4 + 4 * num_streams;
+            uint blocks_offset = 1 + numStreams;
 
             for (uint sidx = 0; sidx < stream_index; sidx++)
             {
                 uint stream_size = this.stream_size(sidx);
-                uint stream_num_pages = this.pages(stream_size, page_size);
-                pages_offset += stream_num_pages * 4;
+                uint stream_num_pages = this.numBlocksRequired(stream_size, blockSize);
+                blocks_offset += stream_num_pages;
             }
 
-            int num_pages = (int)pages(this.stream_size(stream_index), this.page_size);
-            Console.WriteLine(String.Format("num_pages {0}, pages_offset {1}", num_pages, pages_offset));
+            uint num_blocks = numBlocksRequired(this.stream_size(stream_index), this.blockSize);
+            //Console.WriteLine(String.Format("num_pages {0}, pages_offset {1}", num_pages, pages_offset));
 
-            // return struct.unpack("<%dI" % num_pages, self.read(pages_offset, 4*num_pages))
-            uint[] streams = readPageUInts(pages_offset, num_pages);
-            for (int i = 0; i < num_pages; i++)
-            {
-                Console.WriteLine(String.Format("streams[{0}]={1}", i, streams[i]));
-            }
-
+            uint[] streams = readStreamDirectoryUInts(blocks_offset, num_blocks);
             return streams;
         }
     }
@@ -408,6 +307,10 @@ namespace PdbLibrary
 
         public PDBFile(String filepath)
         {
+            if (BitConverter.IsLittleEndian != true)
+            {
+                throw new Exception("Invalid architecture.  This code requires little-endian");
+            }
 
             using (FileStream fp = new FileStream(filepath, FileMode.Open, FileAccess.Read))
             {
@@ -417,33 +320,22 @@ namespace PdbLibrary
 
                 if (n != PdbSignature.signature.Length)
                 {
-                    Console.WriteLine("Invalid PDB signature");
-                    Environment.Exit(-1);
+                    throw new Exception("Invalid PDB signature");
                 }
 
                 for (int i = 0; i < PdbSignature.signature.Length; i++)
                 {
                     if (PdbSignature.signature[i] != bytes[i])
                     {
-                        Console.WriteLine(String.Format("Warning signatures differ at {0}. Expected {1} found {2}", i, PdbSignature.signature[i], bytes[i]));
+                        throw new Exception(
+                            String.Format("Invalid PDB signatures differ at {0}. Expected {1} found {2}",
+                                            i, PdbSignature.signature[i], bytes[i])
+                        );
                     }
                 }
 
-                // load page size and root stream definition
-                uint fileReadOffset = (uint)PdbSignature.signature.Length;
-                uint page_size = ReadHelper.ReadUInt32(fp, fileReadOffset);
-
-                fileReadOffset += 12;
-                uint root_dir_size = ReadHelper.ReadUInt32(fp, fileReadOffset);
-
-                // From llvm.org layout notes
-                fileReadOffset = (uint)PdbSignature.signature.Length + 3 * sizeof(int);
-                uint numDirectoryBytes = ReadHelper.ReadUInt32(fp, fileReadOffset);
-
-                uint numStreamDirectoryBlocks = (uint)Math.Ceiling( (float)numDirectoryBytes / page_size );
-
                 // Create Root stream parser
-                Root root = new Root(fp, page_size, root_dir_size);
+                Root root = new Root(fp);
 
                 // load the PDB stream page
                 uint[] pdb_stream_pages = root.stream_pages(1);
@@ -451,7 +343,7 @@ namespace PdbLibrary
                 // load GUID from PDB stream
                 int guidBytesLength = 4 * 4 + 2 * 2;
                 byte[] guidBytes = new byte[guidBytesLength];
-                fileReadOffset = pdb_stream_pages[0] * page_size;
+                uint fileReadOffset = pdb_stream_pages[0] * root.getBlockSize();
                 fp.Seek(fileReadOffset, SeekOrigin.Begin);
                 fp.Read(guidBytes, 0, guidBytesLength);
 
@@ -461,7 +353,6 @@ namespace PdbLibrary
 
                 byte[] guid_d4 = new byte[8];
                 fp.Read(guid_d4, 0, 8);
-                Console.WriteLine(String.Format("guids: {0}, {1}, {2}, {3}", guid_d1, guid_d2, guid_d3, BitConverter.ToString(guid_d4)));
 
                 // load age from the DBI information
                 // (PDB information age changes when using PDBSTR)
@@ -469,7 +360,7 @@ namespace PdbLibrary
 
                 if (0 < dbi_stream_pages.Length)
                 {
-                    fileReadOffset = dbi_stream_pages[0] * page_size + 2 * 4;
+                    fileReadOffset = dbi_stream_pages[0] * root.getBlockSize() + 2 * 4;
                     uint age = ReadHelper.ReadUInt32(fp, fileReadOffset);
                     this.guid = new GUID(guid_d1, guid_d2, guid_d3, guid_d4, age);
                 }
@@ -485,12 +376,6 @@ namespace PdbLibrary
     }
     public class PEFile
     {
-
-        // Simple PE file parser, that loads two fields used to generate the guid:
-        //
-        //* TimeDateStamp from file header
-        //* SizeOfImage from optional header
-        //
         readonly byte[] PE_SIGNATURE = new byte[] { (byte)'P', (byte)'E', 0, 0 };
         const byte PE_SIGNATURE_POINTER = 0x3C;
 
@@ -520,6 +405,11 @@ namespace PdbLibrary
 
         public PEFile(string filepath)
         {
+            if (BitConverter.IsLittleEndian != true)
+            {
+                throw new Exception("Invalid architecture.  This code requires little-endian");
+            }
+
             using (FileStream fp = new FileStream(filepath, FileMode.Open, FileAccess.Read))
             {
                 // load PE signature offset
@@ -531,16 +421,14 @@ namespace PdbLibrary
                 int n = fp.Read(peSigBytes, 0, 4);
                 if (n != 4)
                 {
-                    Console.WriteLine("Unable to read PE signature");
-                    Environment.Exit(-1);
+                    throw new Exception("Unable to read PE signature");
                 }
 
                 for (int i = 0; i < 4; i++)
                 {
                     if (PE_SIGNATURE[i] != peSigBytes[i])
                     {
-                        Console.WriteLine(
-                            String.Format("PE signature mismatch index {0}, expected{1}, found{2}",
+                        throw new Exception( String.Format("PE signature mismatch index {0}, expected{1}, found{2}",
                                             i, PE_SIGNATURE[i], peSigBytes[i]));
                     }
                 }
